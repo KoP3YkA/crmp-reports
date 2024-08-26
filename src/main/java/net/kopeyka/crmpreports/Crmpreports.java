3a6bae098f7e1e0d6231930df7edd8571f3a9aff
ArteMerlow <artemy.v@mail.ru>
2024-08-26 10:50:03 +0700

    init

---
+++ src/main/java/net/kopeyka/crmpreports/Crmpreports.java
@@ -0,0 +1,17 @@
+package net.kopeyka.crmpreports;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Crmpreports extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
